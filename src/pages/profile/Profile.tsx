5c13aef902a4e2f9b69f182653019a52b87d9672
RamanMilashevich <roma.milashevich@gmail.com>
2025-05-29 14:50:43 +0200

    feat: add user info to profil page

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,8 +1,29 @@
 import React from "react";
-import "./Profile.css";
+import { useAuth } from "@/context/AuthContext";
 
 const Profile = () => {
-  return <div className="profile-page">Customer Profile</div>;
+  const { customer } = useAuth();
+
+  if (!customer) return <p>Loading...</p>;
+
+  const {
+    firstName,
+    lastName,
+    dateOfBirth,
+  } = customer;
+
+  return (
+    <div className="profile-page">
+      <h2>User Profile</h2>
+
+      <section className="personal-info">
+        <h3>Personal Information</h3>
+        <p><strong>First Name:</strong> {firstName}</p>
+        <p><strong>Last Name:</strong> {lastName}</p>
+        <p><strong>Date of Birth:</strong> {dateOfBirth}</p>
+      </section>
+    </div>
+  );
 };
 
 export default Profile;
