f773a33123d01c6bfe1ec10070500b9c2cfb4199
RamanMilashevich <roma.milashevich@gmail.com>
2025-05-29 15:47:48 +0200

    feat: add section with addresses info

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Profile.css";
 import { useAuth } from "@/context/AuthContext";
 
 const Profile = () => {
@@ -10,6 +11,9 @@ const Profile = () => {
     firstName,
     lastName,
     dateOfBirth,
+    addresses,
+    defaultBillingAddressId,
+    defaultShippingAddressId,
   } = customer;
 
   return (
@@ -18,10 +22,43 @@ const Profile = () => {
 
       <section className="personal-info">
         <h3>Personal Information</h3>
-        <p><strong>First Name:</strong> {firstName}</p>
-        <p><strong>Last Name:</strong> {lastName}</p>
-        <p><strong>Date of Birth:</strong> {dateOfBirth}</p>
+        <p className="p-text"><strong>First Name:</strong> {firstName}</p>
+        <p className="p-text"><strong>Last Name:</strong> {lastName}</p>
+        <p className="p-text"><strong>Date of Birth:</strong> {dateOfBirth}</p>
       </section>
+
+     <section className="addresses">
+        <h3>Addresses</h3>
+        {addresses && addresses.length > 0 ? (
+          addresses.map((addr, index) => (
+            <div key={addr.id || index} className="address-card">
+              <p className="p-text">
+                {addr.streetName}, {addr.postalCode}, {addr.city}, {addr.state || ""}, {addr.country}
+              </p>
+
+              {/* Billing */}
+              {addr.id === defaultBillingAddressId && (
+                <p className="address-label">🏷️ Default Billing Address</p>
+              )}
+              {addr.id !== defaultBillingAddressId && (
+                <p className="address-label">❗ This address is not Default Billing Address</p>
+              )}
+
+              {/* Shipping */}
+              {addr.id === defaultShippingAddressId && (
+                <p className="address-label">📦 Default Shipping Address</p>
+              )}
+              {addr.id !== defaultShippingAddressId && (
+                <p className="address-label">❗ This address is not Default Shipping Address</p>
+              )}
+            </div>
+          ))
+        ) : (
+          <p>No addresses found.</p>
+        )}
+      </section>
+
+
     </div>
   );
 };
